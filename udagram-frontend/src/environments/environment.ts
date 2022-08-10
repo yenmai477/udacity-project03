1a4ad05c4bb5f9c7ce8b00ef685907895c08d6f5
yenmai477 <yenmai478@gmail.com>
2022-08-10 16:02:03 +0700

    update reverse proxy url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a23fb89ae6a7840e799573fb51055870-89970179.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
